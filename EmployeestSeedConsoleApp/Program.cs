f7af3beafb0cb15e9d0c370ff33c8bceec055696
SergiyVengrin <sergiyvengrin19@gmail.com>
2022-10-13 18:22:29 +0300

    22: Created AdoDotNet Project

---
+++ EmployeestSeedConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+
+
+string? dbUsername = Environment.GetEnvironmentVariable("DbUsername", EnvironmentVariableTarget.User);
+string? dbPassword = Environment.GetEnvironmentVariable("DbPassword", EnvironmentVariableTarget.User);
+
+string connectionString = $"Host=localhost;Port=5432;Database=CommentsDb;Username={dbUsername};Password={dbPassword}";
+
+
+using (NpgsqlConnection sqlConnection = new NpgsqlConnection(connectionString))
+{
+    await sqlConnection.OpenAsync();
+    Console.WriteLine("Connection Info:");
+    Console.WriteLine($"Connection string: {sqlConnection.ConnectionString}");
+    Console.WriteLine($"Database: {sqlConnection.Database}");
+    Console.WriteLine($"Username: {sqlConnection.UserName}");
+    Console.WriteLine($"Server: {sqlConnection.DataSource}");
+    Console.WriteLine($"Server Version: {sqlConnection.ServerVersion}");
+    Console.WriteLine($"State: {sqlConnection.State}");
+}
