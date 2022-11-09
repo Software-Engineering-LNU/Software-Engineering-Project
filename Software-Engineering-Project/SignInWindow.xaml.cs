85ec413a6d9598a22a1f05db02e8e2c1b23ffd3c
Viktoria <belya.viktoriya@gmail.com>
2022-11-09 16:07:29 +0200

    52: Impelemented pop up messages on validation errors

--- Software-Engineering-Project/SignInWindow.xaml.cs
+++ Software-Engineering-Project/SignInWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +22,42 @@ namespace Software_Engineering_Project
     {
         public SignInWindow()
         {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
+            labelErrorMessage.Visibility = Visibility.Hidden;
+        }
+        private void textBoxEmailTextChanged(object sender, TextChangedEventArgs args)
+        {
+            labelErrorMessage.Visibility = Visibility.Hidden;
+        }
+        private void passwordBoxEmailTextChanged(object sender, RoutedEventArgs args)
+        {
+            labelErrorMessage.Visibility = Visibility.Hidden;
+        }
+        private bool isValidData()
+        {
+            if(!Regex.IsMatch(textBoxEmail.Text, "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"))
+            {
+                labelErrorMessage.Content = "Invalid email input";
+                return false;
+            }
+            if(passwordBoxPassword.Password.Length < 9)
+            {
+                labelErrorMessage.Content = "Min length of password is 8";
+                return false;
+            }
+            return true;
+        }
+        private void buttonSignIn_Clicked(object sender, RoutedEventArgs e)
+        {
+            if(isValidData())
+            {
+                
+            }
+            else
+            {
+                labelErrorMessage.Visibility = Visibility.Visible;
+            }
         }
     }
 }
